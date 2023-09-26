38486a08b8110fda156aa959d0c9c8637ed1bee9
Danilo Rocha <danilosilvasrocha@gmail.com>
2023-09-26 16:16:31 -0300

    Comentarios

--- Controllers/FuncionarioController.cs
+++ Controllers/FuncionarioController.cs
@@ -45,13 +45,11 @@ public class FuncionarioController : ControllerBase
     {
         _context.Funcionarios.Add(funcionario);
         _context.SaveChanges();
-        // TODO: Chamar o método SaveChanges do _context para salvar no Banco SQL
 
         var tableClient = GetTableClient();
         var funcionarioLog = new FuncionarioLog(funcionario, TipoAcao.Inclusao, funcionario.Departamento, Guid.NewGuid().ToString());
 
         tableClient.UpsertEntity(funcionarioLog);
-        // TODO: Chamar o método UpsertEntity para salvar no Azure Table
 
         return CreatedAtAction(nameof(ObterPorId), new { id = funcionario.Id }, funcionario);
     }
@@ -72,14 +70,12 @@ public class FuncionarioController : ControllerBase
         funcionarioBanco.Salario = funcionario.Salario;
         funcionarioBanco.DataAdmissao = funcionario.DataAdmissao;
 
-        // TODO: Chamar o método de Update do _context.Funcionarios para salvar no Banco SQL
         _context.Funcionarios.Update(funcionarioBanco);
         _context.SaveChanges();
 
         var tableClient = GetTableClient();
         var funcionarioLog = new FuncionarioLog(funcionarioBanco, TipoAcao.Atualizacao, funcionarioBanco.Departamento, Guid.NewGuid().ToString());
 
-        // TODO: Chamar o método UpsertEntity para salvar no Azure Table
         tableClient.UpsertEntity(funcionarioLog);
 
         return Ok();
@@ -93,14 +89,12 @@ public class FuncionarioController : ControllerBase
         if (funcionarioBanco == null)
             return NotFound();
 
-        // TODO: Chamar o método de Remove do _context.Funcionarios para salvar no Banco SQL
         _context.Funcionarios.Remove(funcionarioBanco);
         _context.SaveChanges();
 
         var tableClient = GetTableClient();
         var funcionarioLog = new FuncionarioLog(funcionarioBanco, TipoAcao.Remocao, funcionarioBanco.Departamento, Guid.NewGuid().ToString());
 
-        // TODO: Chamar o método UpsertEntity para salvar no Azure Table
         tableClient.UpsertEntity(funcionarioLog);
 
         return NoContent();
